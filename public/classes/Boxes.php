fba1403e034b7ae2b27a71cf4befca4d1e47709b
Lucas Regalar <lucas.regalar@palasthotel.de>
2025-07-01 09:49:37 +0200

    fix: add html_box

--- public/classes/Boxes.php
+++ public/classes/Boxes.php
@@ -21,6 +21,7 @@ class Boxes extends _Component
 		require( $dir . '/../core/classes/wordpress/grid_wp_shortcode_box.php' );
 		require( $dir . '/../core/classes/wordpress/grid_search_form_box.php' );
 		require( $dir . '/../core/classes/wordpress/grid_video_box.php' );
+        require( $dir . '/../core/classes/wordpress/grid_html_box.php' );
 
 		// box with all input types for debugging and checking if inputs are working as expected
 		if(WP_DEBUG) require( $dir . '/../core/classes/wordpress/grid_debug_box.php' );
